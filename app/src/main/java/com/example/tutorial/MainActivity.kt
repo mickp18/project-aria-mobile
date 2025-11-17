97a5deba62c812434d1559ef46084d2911938d54
mickp18 <miperseo@gmail.com>
2025-11-17 19:51:21 +0100

    initial commit

---
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.tutorial
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+class MainActivity : AppCompatActivity() {
+    private lateinit var startButton : Button
+    private lateinit var stopButton : Button
+    private lateinit var webSocketClient: WebSocketClient
+
+
+    private val socketListener = object : WebSocketClient.SocketListener {
+        override fun onMessage(message: String) {
+            Log.e("socketCheck onMessage", message)
+        }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        startButton = findViewById(R.id.startButton)
+        stopButton = findViewById(R.id.stopButton)
+        stopButton.isEnabled = false
+
+
+        startButton.setOnClickListener {
+            startButton.isEnabled = false
+            stopButton.isEnabled = true
+            webSocketClient = WebSocketClient.getInstance()
+            // TODO: REPLACE WITH ACTUAL IP ADDRESS OF YOUR DEVICE
+            webSocketClient.setSocketUrl("ws://10.0.2.2:8080")
+            webSocketClient.setListener(socketListener)
+        }
+
+        stopButton.setOnClickListener{
+            startButton.isEnabled = true
+            stopButton.isEnabled = false
+
+        }
+
+    }
+}
